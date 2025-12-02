d7a2103228ab1b5cc65bef7deb3f7abe4383e028
Andersonmia2222 <mia.anderson2026@gbwl.org>
2025-12-02 13:28:28 +0000

    update

--- Main.java
+++ Main.java
@@ -7,21 +7,23 @@
        String command = ""; // stores the user's choices
 
        // STEP #1: Describe current scenatio
-       System.out.println(" You are at a mall in long island.");
+       System.out.println(" You are at a mall in Long island");
+      System.out.println("It's crowded, loud, and smells like Auntie Annes pretzels.");
+
         
        // STEP #2: Get user commands 
        command = getCommand("Do you want to start upstairs or downstairs ");
       // STEP #3: Select a path based on user's command 
-      if (command.equals("e")) {
-         // NEXT BRANCH...
-         System.out.println("You reach the Caroisel.");
-
+      if (command.equals("e"))
+          {
+System.out.println("You ride the escalator up. At the top, you see the carousel spinning with kids screaming. A worker waves at you.");
+            System.out.println("You reach the carousel.");       
    }
    else if (command.equals("n")) {
-      System.out.println("You will see a wedding at Tavern on the Green");
+      System.out.println("You walk downstairs to eat food");
    }
    else{
-      System.out.println("GAME OVER! You got lost in the time Warner center shopping for hours. Press RUN to replay.");
+      System.out.println("GAME OVER! You got lost in the time Zara and got lost for hours. Press RUN to replay.");
    }
    
     } // END OF MAIN METHOD
